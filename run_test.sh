34a00403c1ac7e470a49d378919351f0f0e81f96
liuli0212 <liuli0212@users.noreply.github.com>
2026-02-22 23:26:00 +0800

    Remove test scripts and patches

--- run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:~/.cargo/bin
-cargo run << 'IN'
-ls -l
-exit
-IN
--- run_test2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:~/.cargo/bin
-cargo run << 'IN'
-please run a bash command to echo hello world
-exit
-IN
