11f2f2626afc099ef33ed4f809cf346b176bb192
liuli0212 <liuli0212@users.noreply.github.com>
2026-02-23 06:28:14 +0800

    chore: cleanup

--- test_llm_response.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:~/.cargo/bin
-cargo run << 'IN'
-Write a bash script to analyze the rust code in the src/ directory using clippy and save the output to report.txt
-exit
-IN
